d662b5c01c409c491741c8f78f4183da3cf1fd2e
charlescostello <charlescc9@yahoo.com>
2017-01-30 16:42:53 -0800

    Adding initial files

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by charlescostello on 1/30/17.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+}
